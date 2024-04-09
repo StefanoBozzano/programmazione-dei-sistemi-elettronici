1413a1d8fb7c441ccb4a635fe5b786ece9e77521
Stefano Bozzano <bste02@gmail.com>
2024-04-09 06:35:51 +0200

    comm2

--- stupid_math.c
+++ stupid_math.c
@@ -3,7 +3,7 @@
 float mean(int a, int b){
     return (a + b) / 2;
 }
-//comm1
+//comm2
 complex_float_t commplex_mean(complex_int_t a, complex_int_t b){
     complex_float_t res = {0, 0};
     res.re = mean(a.re, b.re);
